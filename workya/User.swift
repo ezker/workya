4c88ed0537aa3f0aef6edc13a7fea578ce812a61
reynaldo.martindelC <reynaldo.martindelc@redudg.udg.mx>
2019-03-22 15:28:02 -0600

    modelo  User

---
+++ workya/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  workya
+//
+//  Created by Reynaldo Martin del Campo on 3/22/19.
+//  Copyright © 2019 swiftbus. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    
+    var name: String
+    var lastName: String
+    var picture: String
+    var city: String
+    var schedule: String
+    var score: Float
+    var comments: [String]
+    var services: [String]
+    var portfolio: [String]
+    
+    
+}
